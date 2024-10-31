6d848f66261e79fb259bd39450e020fff0b9828a
M1tras <108801657+M1tras@users.noreply.github.com>
2024-10-31 10:53:12 +0200

    Create function.py

---
+++ function.py
@@ -0,0 +1,24 @@
+"""Function examples."""
+
+
+# func()
+def func():
+    return print("I´m inside the function")
+    
+
+# my_name_is(name)
+def my_name_is(name: str):
+    return print(f"My name is {name}") 
+
+# sum_six(num)`
+def sum_six(num: int):
+    arvutus = num + 6
+    return arvutus
+
+# sum_numbers()
+def sum_numbers(a, b):
+    return a + b
+    
+# usd_to_eur()
+def usd_to_eur(money_dollar):
+    return money_dollar * 0.8
